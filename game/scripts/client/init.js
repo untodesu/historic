ee664bd670652922a107f0e33f051ddbf8678527
undnull <41549137+undnull@users.noreply.github.com>
2021-11-13 02:10:27 +0500

    Oops

--- game/scripts/client/init.js
+++ game/scripts/client/init.js
@@ -2,5 +2,5 @@ CVar.set("r.fov", "103.000000");
 CVar.set("r.znear", "0.010000");
 CVar.set("r.shadows", "1");
 CVar.set("r.zfar", "256.000000");
-CVar.set("r.shadowmapres", "2048");
+CVar.set("r.shadowmapres", "4096");
 
